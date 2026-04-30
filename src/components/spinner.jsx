0c510f69bc1625d165cf9268b5e6cbe01991b3b3
eduardsalas <edusalasmurqest.utn.ac.cr>
2026-04-29 19:54:59 -0600

    Se  creo el componente Spinner

---
+++ src/components/spinner.jsx
@@ -0,0 +1,9 @@
+export default function SpinnerCustom() {
+  return (
+    <div className="text-center my-4">
+      <div className="spinner-border text-primary" role="status">
+        <span className="visually-hidden">Cargando...</span>
+      </div>
+    </div>
+  );
+}

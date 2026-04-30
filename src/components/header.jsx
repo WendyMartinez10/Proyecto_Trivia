8e689fb9efd9abc122d214f574c99466a0096835
Felipe0220 <lusalaspe@est.utn.ac.cr>
2026-04-29 20:04:06 -0600

    Se creo el componente header

---
+++ src/components/header.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4">
+      <div className="container-fluid">
+
+        <span className="navbar-brand fw-bold">
+          🧠BrainQuiz
+        </span>
+
+        <div className="d-flex align-items-center gap-3">
+        </div>
+
+      </div>
+    </nav>
+  );
+}
